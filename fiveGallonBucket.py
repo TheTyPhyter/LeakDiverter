216648e3f18dc28431ef5241d394f74bd7168a3b
TheTyPhyter <45110834+TheTyPhyter@users.noreply.github.com>
2022-10-03 23:10:01 -0700

    Update fiveGallonBucket.py
    Added command line args. Coded cleaned up by Zaldimmar.

--- fiveGallonBucket.py
+++ fiveGallonBucket.py
@@ -5,60 +5,72 @@ useful for downloading entire data leaks.
 Painfully slow, but it's python and TOR and Im bad at scripting so idk what you expected...
 Uncomment print lines for debugging.
 """
+
+import sys
 import http
 import os.path
 import requests
 from bs4 import BeautifulSoup
 
-url = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.onion'
+try:
+    url = sys.argv[1]
 
-os.startfile('<PATH TO tor.exe>')
+    avoidURLs = ["?C=N;O=D", "?C=D;O=A", "?C=S;O=A", "?C=M;O=A", "Parent Directory"]
 
-def sessionHandler(site):
-    session = requests.session()
-    session.proxies = {'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}
-    r = session.get(site)
-    return r
+    os.startfile('C:\\Users\\TyPhyter\\Desktop\\Tor Browser\\Browser\\TorBrowser\\Tor\\tor.exe')
 
-
-def scraper(page):
-    soup = BeautifulSoup(page.content, 'html.parser', from_encoding="iso-8859-1")
-    links = soup.find_all("a")
-    filePath = page.url.split('departments/')[-1].replace('%20', ' ')
-    for link in links:
-        try:
-            if link.text.endswith('/'):
-                if not os.path.exists(filePath + link.text):
-                    print('Making new directory: ', filePath + link.text)
-                    os.mkdir(filePath + link.text)
-                #print('a', link.text)
-                nextdir = sessionHandler(page.url + link.text)
-                scraper(nextdir)
-            else:
-                #print('b', link.text)
-                if not os.path.exists(filePath + link.text):
-                    print('Saving new file')
-                    downloadPage(page.url + link.text, filePath)
-                else:
-                    print('The file ', "\'\'", filePath + link.text, "\'\'", ' already exists, moving on...')
-        except requests.exceptions.ConnectionError:
-            print('Error retrieving ', link.text, '. Trying again...')
-            scraper(sessionHandler(page.url + link.text))
-
-        except http.client.RemoteDisconnected:
-            print('Server ended the session, retrying...')
-            scraper(sessionHandler(page.url + link.text))
+    def sessionHandler(site):
+        session = requests.session()
+        session.proxies = {'http': 'socks5h://localhost:9050', 'https': 'socks5h://localhost:9050'}
+        r = session.get(site)
+        return r
 
 
-def downloadPage(file, path):
-    outFile = file.split('/')[-1]
-    #print('c', outFile)
-    with sessionHandler(file) as r:
-        #print('d', path)
-        with open(path + outFile, 'wb') as f:
-            f.write(r.content)
-            f.close()
+    def scraper(page):
+        soup = BeautifulSoup(page.content, 'html.parser', from_encoding="iso-8859-1")
+        links = soup.find_all("a")
+        fileCount = 0
+        for link in links:
+            if link['href'] not in avoidURLs and link.text not in avoidURLs:
+                filePath = page.url.split(url)[-1].replace('%20', ' ')
+                fullFilePath = filePath + link.text
+                fUrl = page.url + link.text
+                try:
+                    if link.text.endswith('/'):
+                        if not os.path.exists(fullFilePath):
+                            print('Making new directory: ', fullFilePath)
+                            os.mkdir(fullFilePath)
+                        # print('a', link.text)
+                        nextdir = sessionHandler(fUrl)
+                        scraper(nextdir)
+                    else:
+                        #print('b', link.text)
+                        if not os.path.exists(fullFilePath):
+                            print('Saving new file')
+                            downloadPage(fUrl, fullFilePath)
+                            fileCount += 1
+                            print(fileCount, 'files donwloaded')
+                        else:
+                            print("The file '", fullFilePath, "' already exists, moving on...'")
+                except requests.exceptions.ConnectionError:
+                    print('Error retrieving ', link.text, '. Trying again...')
+                    scraper(sessionHandler(fUrl))
+
+                except http.client.RemoteDisconnected:
+                    print('Server ended the session, retrying...')
+                    scraper(sessionHandler(fUrl))
 
 
-scraper(sessionHandler(url))
+    def downloadPage(url, file):
+        print('Downloading: ', file)
+        with sessionHandler(url) as r:
+            #print('d', path)
+            with open(file, 'wb') as f:
+                f.write(r.content)
+                f.close()
+    scraper(sessionHandler(url))
 
+except IndexError:
+    print("Welcome to the LeakDiverter.\n Run again with a valid URL [including(especially)] .onion URLsto an"
+          " open directory\n"
+          "'usage: python3 LeakDiverter.py <valid URL>'")
